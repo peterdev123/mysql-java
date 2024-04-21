b971e8335bed019fd3ae27ec1ae4e9aa85bc3a0d
Peter <xsterfo456@gmail.com>
2024-04-21 18:55:24 +0800

    Modified

--- src/main/java/com/example/mysql/MainApplication.java
+++ src/main/java/com/example/mysql/MainApplication.java
@@ -12,7 +12,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene((Parent)fxmlLoader.load(), 436.0, 375.0);
+        Scene scene = new Scene((Parent)fxmlLoader.load(), 500.0, 500.0);
 //        scene.getStylesheets().add(this.getClass().getResource("login.css").toExternalForm());
         stage.setTitle("Login");
         stage.setScene(scene);
